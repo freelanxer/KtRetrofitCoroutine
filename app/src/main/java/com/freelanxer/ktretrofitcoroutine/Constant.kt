a9fd7a9c2c35ffd58804884b99c7c4c8de7dccae
Daniel <kuanshenwang@gmail.com>
2023-11-01 18:17:22 +0800

    Add log functions

--- app/src/main/java/com/freelanxer/ktretrofitcoroutine/Constant.kt
+++ app/src/main/java/com/freelanxer/ktretrofitcoroutine/Constant.kt
@@ -1,5 +1,13 @@
 package com.freelanxer.ktretrofitcoroutine
 
+import android.util.Log
+
 object Constant {
     val DEBUG = BuildConfig.BUILD_TYPE.contentEquals("debug")
+    private const val TAG = "KT_RETROFIT"
+
+    fun logD(msg: String, throwable: Throwable? = null) = Log.d(TAG, msg, throwable)
+    fun logI(msg: String, throwable: Throwable? = null) = Log.i(TAG, msg, throwable)
+    fun logW(msg: String, throwable: Throwable? = null) = Log.w(TAG, msg, throwable)
+    fun logE(msg: String, throwable: Throwable? = null) = Log.e(TAG, msg, throwable)
 }
